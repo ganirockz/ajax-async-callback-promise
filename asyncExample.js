b23cfdb34a30db77167160ada1a0108a71d5bc6d
ganirockz <ganeshrapeti8@gmail.com>
2020-11-24 20:01:39 +0530

    [Ganesh]Added: ability to view employee data from JSON-Server using AJAX

--- asyncExample.js
+++ asyncExample.js
@@ -1,6 +1,6 @@
 function showTime(){
     const date = new Date();
-    return date.getHours()+"Hrs:"+date.getMinutes()+"Mins:"+date.getSeconds()+"Secs";
+    return date.getHours()+"Hrs:"+date.getMinutes()+"Mins:"+date.getSeconds()+"Secs:";
 }
 
 function showSessionExpire(){
@@ -10,4 +10,3 @@ function showSessionExpire(){
 console.log("Acitivity-A: Trigerring Activity-B at "+showTime());
 setTimeout(showSessionExpire, 5000);
 console.log("Activity-A: Triggered Activity-B at "+showTime());
-
